0554c4ef43593f95c5f35500ce01eb893921d0d5
ono5 <kuroneko_head_lord@yahoo.co.jp>
2018-11-23 18:59:35 +0900

    Setting parameter to pycharm and writing script path on README.md

--- tests/selenium_code/flask/tests/acceptance/steps/navigation.py
+++ tests/selenium_code/flask/tests/acceptance/steps/navigation.py
@@ -1,9 +1,11 @@
 from behave import *
 from selenium import webdriver
 
+#  matherの正規表現をサポートする
 use_step_matcher('re')
+
 
 @given('I am on the homepage')
 def step_impl(context):
-    browser = webdriver.Chrome()
+    browser = webdriver.Chrome("/Users/hono/Desktop/python_cookcode/tests/selenium_code/flask/driver/chromedriver 2")
     browser.get('http://127.0.0.1:5000')
