6ed23be2fe9e96e0d90f3067b4b7e807e437d69a
ono5 <kuroneko_head_lord@yahoo.co.jp>
2018-11-24 16:23:34 +0900

    Complete first test code

--- README.md
+++ README.md
@@ -14,7 +14,15 @@ Gherkin
 - pycharm にインストール
 - テストシナリオの記述に使用する
 
+behave(https://behave.readthedocs.io/en/latest/tutorial.html)
 behave package
 pip install behave
-  
+
+Pycharmの設定
+Python を押下
+/Users/hono/anaconda3/envs/python_cookcode/bin/behave
+/Users/hono/Desktop/python_cookcode/tests/selenium_code/flask/tests/acceptance
+
+ chmod +x cmd.sh 
+
 ```
--- tests/selenium_code/flask/tests/acceptance/steps/navigation.py
+++ tests/selenium_code/flask/tests/acceptance/steps/navigation.py
@@ -4,8 +4,16 @@ from selenium import webdriver
 #  matherの正規表現をサポートする
 use_step_matcher('re')
 
+webdriver_path = "/Users/hono/Desktop/python_cookcode/tests/selenium_code/flask/driver/chromedriver 2"
+
 
 @given('I am on the homepage')
 def step_impl(context):
-    browser = webdriver.Chrome("/Users/hono/Desktop/python_cookcode/tests/selenium_code/flask/driver/chromedriver 2")
-    browser.get('http://127.0.0.1:5000')
+    context.browser = webdriver.Chrome(webdriver_path)
+    context.browser.get('http://127.0.0.1:5000')
+
+
+@then('I am on the blog page')
+def setp_impl(context):
+    expected_url = 'http://127.0.0.1:5000/blog'
+    assert context.browser.current_url == expected_url
