99059fa09b8b196ccba5e6194d3ff7fdff1a7e8c
ono5 <kuroneko_head_lord@yahoo.co.jp>
2018-12-08 17:24:09 +0900

    Verifying everythin works

--- tests/BDD/tests/acceptance/steps/navigation.py
+++ tests/BDD/tests/acceptance/steps/navigation.py
@@ -6,6 +6,6 @@ use_step_matcher('re')
 
 @given('I am on the homepage')
 def step_impl(context):
-    browser= webdriver.Chrome()
+    browser= webdriver.Firefox()
     browser.get('http://127.0.0.1:5000')
 
