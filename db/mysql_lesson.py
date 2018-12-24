da1acf776e40fa6c115db41b88e53cb62d44189b
ono5 <kuroneko_head_lord@yahoo.co.jp>
2018-12-24 22:28:41 +0900

    Comment in mysql

--- db/mysql_lesson.py
+++ db/mysql_lesson.py
@@ -1,31 +1,33 @@
 import mysql.connector
 
-# conn = mysql.connector.connect(host='127.0.0.1', user='root', password='')
-#
-# cursor = conn.cursor()
-#
-# cursor.execute(
-#     'CREATE DATABASE test_mysql_database'
-# )
-#
-# cursor.close()
-# conn.close()
+# Create Database
+conn = mysql.connector.connect(host='127.0.0.1', user='root', password='')
+
+cursor = conn.cursor()
+
+cursor.execute(
+    'CREATE DATABASE test_mysql_database'
+)
+
+cursor.close()
+conn.close()
 
 
+# Managing Database
 conn = mysql.connector.connect(host='127.0.0.1', user='root', password='', database='test_mysql_database')
 
 cursor = conn.cursor()
 
-# cursor.execute(
-#     'CREATE TABLE persons('
-#     'id int NOT NULL AUTO_INCREMENT,'
-#     'name varchar(14) NOT NULL,'
-#     'PRIMARY KEY(id))'
-# )
+cursor.execute(
+    'CREATE TABLE persons('
+    'id int NOT NULL AUTO_INCREMENT,'
+    'name varchar(14) NOT NULL,'
+    'PRIMARY KEY(id))'
+)
 
-# cursor.execute('INSERT INTO persons(name) values("Mike")')
+cursor.execute('INSERT INTO persons(name) values("Mike")')
 
-# conn.commit()
+conn.commit()
 
 cursor.execute('SELECT * FROM persons')
 for row in cursor:
